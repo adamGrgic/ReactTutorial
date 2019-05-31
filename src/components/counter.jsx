2a90646a7d67241c10ddfe4d51dbf38c2def1540
Adam <argrgic@gmail.com>
2019-05-30 19:06:02 -0500

    Created basic hello world message using a different component. connected through index.js.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,3 +1,20 @@
 import React, { Component } from 'react';
 
-class
+class Counter extends Component {
+    render() {
+        state = {
+            count: 0,
+            address: {
+                street: ''
+            }
+        }
+        return (
+            <React.Fragment>
+                <span> Hello World</span>
+                <button>Increment</button>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default Counter;
