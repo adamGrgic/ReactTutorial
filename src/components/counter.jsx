87f135a02845952a5b23f812d7b5e0bdbccc106f
Adam <argrgic@gmail.com>
2019-06-10 15:38:42 -0500

    stylized count button.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        
 
         };
     render() {
-    
-    
         return (
             <React.Fragment>
-                <span> {this.formatCount()} </span>
-                <button>Increment</button>
+               <span className="badge badge-primary">{this.formatCount()}</span> 
+               <button>Increment</button>
             </React.Fragment>
         );
     }
